e64aa2aae36adb705c9af022e4bf2bcf68f0e19f
hs102 <herotato108@gmail.com>
2024-03-12 20:29:19 +0300

    this was commited on git

--- eex1.php
+++ eex1.php
@@ -4,6 +4,9 @@ $color = ["red", "green", "blue", "yellow", "purple", "orange"];
 
 for ($i = 1 ; $i <= 6 ; $i++)
 
+// this was updated on git bash
+
+
 {
     $y=$i-1;
     echo  "<h$i style ='color:$color[$y]'> Hello Php</h$i>";
